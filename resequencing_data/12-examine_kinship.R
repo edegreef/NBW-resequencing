96587804eda227d3a16f8348e57a3a68edb88778
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2021-02-04 12:23:15 -0600

    Add files via upload

---
+++ resequencing_data/12-examine_kinship.R
@@ -0,0 +1,73 @@
+# looking at kinship estimates
+
+library(plinkQC)
+
+setwd("C:/Users/Evelien de Greef/Dropbox/NBW-me/reseq_newsnps/kinship")
+
+directory="C:/Users/Evelien de Greef/Dropbox/NBW-me/reseq_newsnps/kinship"
+files="NBW_platypus_SNPs.filter1.filter2.ID.autosomes.LDpruned.rm8highmiss" ##for .genome and .imiss
+  
+png("IBD_autosomes_LDpruned_rm8highmiss_plinkQC_plot.png", w=1000, h=500, res=100)
+evaluate_check_relatedness(
+  qcdir=directory,
+  name=files,
+  highIBDTh = 0.1875,
+  imissTh = 0.03,
+  interactive = FALSE,
+  verbose = FALSE
+)
+dev.off()
+ 
+# extract values for the related individuals from the .genome file (filtering by pi_hat values)
+IBD <- read.table(paste(directory,"/", files,".genome", sep=""), header=T)
+IBD_order_PH <- IBD[order(IBD[,10], decreasing=TRUE),]
+write.csv(IBD_order_PH, "IBD_autosomes_LDpruned_rm8highmiss_PIHAT.csv")
+
+
+# PI_HAT value reference:
+# 1st degree relative: 0.5
+# 2nd degree relative: 0.25
+# 3rd degree relative: 0.125
+
+# Z score references:
+# Z0=P(IBD=0)
+# Z1=P(IBD=1)
+# Z2=P(IBD=2)
+# unrelated indiv, Z0=1
+# unrelated indiv, Z2=0
+# unrelated indiv, ratio <2 ish
+
+
+# Can also use SNPRelate to look at relatedness (code below from Matt Thorstensen)
+# Code to install SNPRelate
+if (!requireNamespace("BiocManager", quietly=TRUE))
+  install.packages("BiocManager")
+BiocManager::install("SNPRelate")
+BiocManager::install("gdsfmt")
+BiocManager::install("SeqArray")
+library(gdsfmt)
+library(SNPRelate)
+
+vcf.fn <- "C:/Users/Evelien de Greef/Dropbox/NBW-me/reseq_newsnps/kinship/NBW_platypus_SNPs.filter1.filter2.ID.autosomes.LDpruned.vcf"
+snpgdsVCF2GDS(vcf.fn, "nbw.gds", method="biallelic.only")
+
+snpgdsSummary("nbw.gds")
+
+genofile <- SNPRelate::snpgdsOpen("nbw.gds")
+
+# Getting a list of the individuals in the SNP relate dataset
+sample.id <- read.gdsn(index.gdsn(genofile, "sample.id"))
+
+# Using the LD-pruned SNPs for kinship analysis using the method of moments (MoM)
+# Based on the guide provided in the vignette here: https://bioconductor.org/packages/release/bioc/vignettes/SNPRelate/inst/doc/SNPRelateTutorial.html#estimating-ibd-using-plink-method-of-moments-mom
+ibd <- snpgdsIBDMoM(genofile, sample.id=NULL, snp.id=NULL,
+                    maf=0.05, missing.rate=0.05, num.thread=2, autosome.only = FALSE)
+# Make a data.frame. This contains pairwise relatedness measures by the method of moments.
+ibd.coeff <- snpgdsIBDSelection(ibd)
+head(ibd.coeff)
+
+plot(ibd.coeff$k0, ibd.coeff$k1, xlim=c(0,1), ylim=c(0,1),
+     xlab="k0", ylab="k1", main="YRI samples (MoM)")
+lines(c(0,1), c(1,0), col="red", lty=2)
+
+snpgdsClose("nbw.gds")
---
+++ resequencing_data/13-final_snp_prep.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# 1) Remove individuals in kin pairs (one in each pair) and prep files for analyses
+snps=NBW_platypus_SNPs.filter1.filter2.ID.autosomes.LDpruned
+snps_out=NBW_platypus_SNPs.filter1.filter2.ID.autosomes.LDpruned.n36
+list=remove_8highmiss_5kinpair
+
+# remove individuals from list (high missingness and kin pair)
+vcftools --vcf $snps.vcf --remove $list --recode --recode-INFO-all --out $snps_out
+mv $snps_out.recode.vcf $snps_out.vcf
+
+# convert to bim/bed/fam, using these files for most pop analyses
+/home/degreefe/programs/plink --allow-extra-chr --make-bed --vcf $snps_out.vcf --set-missing-var-ids @:#\$1,\$2 --out $snps_out
+
+# increase max missingness filter to 0.1 (0.9 for vcftools) to use in LEA and Rtsne
+vcftools --vcf $snps_out.vcf --max-missing 0.9 --recode --recode-INFO-all --out $snps_out.miss01
+mv $snps_out.miss01.recode.vcf $snps_out.miss01.vcf
+
+# convert to ped/map (for LEA program)
+/home/degreefe/programs/bcftools-1.9/bcftools view -H $snps_out.miss01.vcf | cut -f 1 | uniq | awk '{print $0"\t"$0}' > chrom-map.txt
+vcftools --vcf $snps_out.miss01.vcf --plink --chrom-map chrom-map.txt --out $snps_out.miss01
+
+# imputing remaining snps (for Rtsne program). LD-pruned file probably ok for tsne, but if want to impute snps for other things may need to do so with not-pruned file.
+java -jar $EBROOTBEAGLE/beagle.jar gt=NBW_platypus_SNPs.filter1.filter2.ID.autosomes.LDpruned.n36.miss01.vcf iterations=20 out=NBW_platypus_SNPs.filter1.filter2.ID.autosomes.LDpruned.n36.miss01.imputed
+
+
+# 2) sex-linked snps
+Xsnps=NBW_platypus_SNPs.filter1.filter2.ID.Xchr
+Ysnps=NBW_platypus_SNPs.filter1.filter2.ID.Ychr
+list_females=females
+list_males=males
+
+# prune snps
+./LD_pruning.sh $Xsnps.vcf
+./LD_pruning.sh $Ysnps.vcf
+
+# make datasets to separate females and males for X and Y
+vcftools --vcf $Xsnps.LDpruned.vcf --keep $list_females --recode --recode-INFO-all --out $Xsnps.LDpruned.femalesX
+mv $Xsnps.LDpruned.femalesX.recode.vcf $Xsnps.LDpruned.femalesX.vcf
+
+vcftools --vcf $Ysnps.LDpruned.vcf --keep $list_males --recode --recode-INFO-all --out $Ysnps.LDpruned.malesY
+mv $Ysnps.LDpruned.malesY.recode.vcf $Ysnps.LDpruned.malesY.vcf
+
+# convert to bim/bed/fam
+/home/degreefe/programs/plink --allow-extra-chr --make-bed --vcf $Xsnps.LDpruned.femalesX.vcf --set-missing-var-ids @:#\$1,\$2 --out $Xsnps.LDpruned.femalesX
+
+/home/degreefe/programs/plink --allow-extra-chr --make-bed --vcf $Ysnps.LDpruned.malesY.vcf --set-missing-var-ids @:#\$1,\$2 --out $Ysnps.LDpruned.malesY
