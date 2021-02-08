4b201bc59892de8d9e8d99b2af14224686e71bde
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2021-02-08 13:55:18 -0600

    Update 06-edit_label_ID.sh

--- resequencing_data/06-edit_label_ID.sh
+++ resequencing_data/06-edit_label_ID.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# script for editing labels in the vcf, shortening sample ID and adding "contig" to each contig number
+# script for editing labels in the vcf, shortening sample ID and adding "scaffold" to each scaffold number
 
 snps=/home/degreefe/NBW/reseq_newsnps/NBW_platypus_SNPs.filter1.filter2
 
@@ -19,17 +19,17 @@ awk '{print $NF}' FS=/ vcf_sampleID > vcf_sampleID_shortened
 /home/degreefe/programs/bcftools-1.9/bcftools reheader -s vcf_sampleID_shortened $snps.vcf.gz -o $snps.sample.vcf.gz
 tabix -p vcf $snps.sample.vcf.gz
 
-# 2) add the word "contig" to each contig number (i.e. "3" will turn into "contig3"). This will help for some downstream programs (such as plink).
-# make list of current contig numbers
-/home/degreefe/programs/bcftools-1.9/bcftools index --stats $snps.sample.vcf.gz > $snps.contigstat
-awk '{print $1}' $snps.contigstat > contig_list
-sed -e 's/^/contig/' contig_list > contig_list_newname
-paste contig_list contig_list_newname > contig_rename.txt
-rm contig_list
-rm contig_list_newname
+# 2) add the word "scaffold" to each scaffold number (i.e. "3" will turn into "scaffold3"). This will help for some downstream programs (such as plink).
+# make list of current scaffold numbers
+/home/degreefe/programs/bcftools-1.9/bcftools index --stats $snps.sample.vcf.gz > $snps.scaffoldstat
+awk '{print $1}' $snps.scaffoldstat > scaffold_list
+sed -e 's/^/scaffold/' scaffold_list > scaffold_list_newname
+paste scaffold_list scaffold_list_newname > scaffold_rename.txt
+rm scaffold_list
+rm scaffold_list_newname
 
-# annotate vcf to update contig names
-/home/degreefe/programs/bcftools-1.9/bcftools annotate --rename-chrs contig_rename.txt $snps.sample.vcf.gz -o $snps.IDtemp.vcf
+# annotate vcf to update scaffold names
+/home/degreefe/programs/bcftools-1.9/bcftools annotate --rename-chrs scaffold_rename.txt $snps.sample.vcf.gz -o $snps.IDtemp.vcf
 rm $snps.sample*
 
 # also remove the ##contig lines since contig names are updated now and they take up a lot of unecessary space in header info 
