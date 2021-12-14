fade7812773f1ae19bc20cf488a0e476a2187dfc
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2021-12-14 11:31:51 -0600

    Update LD_pruning.sh

--- resequencing_data/LD_pruning.sh
+++ resequencing_data/LD_pruning.sh
@@ -3,16 +3,16 @@
 snps=$1
 
 # LD pruning SNPs
-/home/degreefe/programs/plink --vcf $snps.vcf --allow-extra-chr --indep-pairwise 50 5 0.2 --set-missing-var-ids @:#\$1,\$2 --out pruned.50window.r0.2
+/home/degreefe/programs/plink --vcf $snps.vcf --allow-extra-chr --indep-pairwise 50 5 0.5 --set-missing-var-ids @:#\$1,\$2 --out pruned.50window.r0.5
 
 # preparing the prune-in file to filter vcf
-sed 's/:/\t/g' pruned.50window.r0.2.prune.in > temp.in
+sed 's/:/\t/g' pruned.50window.r0.5.prune.in > temp.in
 sed 's/...$//' temp.in > temp2.in
-mv temp2.in list.pruned.50window.r0.2.prune.in
+mv temp2.in list.pruned.50window.r0.5.prune.in
 rm temp.in
 
 # prune the snps
-vcftools --vcf $snps.vcf --positions list.pruned.50window.r0.2.prune.in --recode --recode-INFO-all --out $snps.LDpruned
+vcftools --vcf $snps.vcf --positions list.pruned.50window.r0.5.prune.in --recode --recode-INFO-all --out $snps.LDprunedr05
 
 # renaming to remove the "recode" thing
-mv $snps.LDpruned.recode.vcf $snps.LDpruned.vcf
+mv $snps.LDprunedr05.recode.vcf $snps.LDprunedr05.vcf
