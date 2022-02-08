b0c2796da49684b928539fbd68a1917b4c45687b
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2022-02-08 15:24:15 -0600

    Update roh_plot.R

--- pop_analyses/roh_plot.R
+++ pop_analyses/roh_plot.R
@@ -5,24 +5,28 @@
 
 # then plot in R!
 library(ggplot2)
+setwd("C:/Users/Evelien de Greef/Dropbox/NBW-me/NBW_oct2021_updated/snps_2M/roh/")
 
-JM <- read.table("IC.hom.indiv", header=T)
-WNA <- read.table("ARLBNF.hom.indiv", header=T)
-SS <- read.table("SS.hom.indiv", header=T)
+IC <- read.table("ICtest.hom.indiv", header=T)
+ARLBNF <- read.table("ARLBNFtest.hom.indiv", header=T)
+SS <- read.table("SStest.hom.indiv", header=T)
 
-JM$region <- "JM"
-WNA$region <- "WNA"
+IC$region <- "IC"
+ARLBNF$region <- "ARLBNF"
 SS$region <- "SS"
 
-merge <- rbind(JM, WNA, SS)
+merge <- rbind(IC, ARLBNF, SS)
+#merge <- rbind(IC, SS)
 
 set <- merge[, c("NSEG", "KB", "region")]
 
-roh <- ggplot(data=set, aes(x=KB,y=NSEG))+
+set$MB <- (set$KB)/1000
+
+roh <- ggplot(data=set, aes(x=MB,y=NSEG))+
   geom_point(aes(color=region), size=4, alpha=0.7)+
   theme_classic()+
   labs(color= "Region")+
-  xlab("Total length of ROHs (kb)")+
+  xlab("Total length of ROHs (Mb)")+
   ylab("Number of ROHs")+
   theme(legend.position="none", text=element_text(family="serif"))+
   scale_colour_manual(values = c("#ABD9E9", "#4575B4","#A50026"))
@@ -40,19 +44,30 @@ bar_NSEG <- ggplot(set, aes(x=reorder(region,NSEG), y=NSEG, fill=region))+
   scale_fill_manual(values = c("#ABD9E9", "#4575B4","#A50026"))
 bar_NSEG
 
-# adjust ARLBNF label to WNA
-labels <- c("JM", "WNA", "SS")
+# adjust site labels IC -> JM, and ARLBNF -> WNA)
 
-bar_KB <- ggplot(set, aes(x=reorder(region,KB), y=KB, fill=region))+
+labels <- c("JM", "WNA", "SS") 
+bar_KB <- ggplot(set, aes(x=reorder(region,MB), y=MB, fill=region))+
   geom_violin(width=0.8, lwd=1)+
   geom_boxplot(width=.1, fill="white", alpha=1)+
   #geom_point(shape = 21, colour = "black", fill = "gray",size=1.5)+
   theme_classic()+
-  ylab("Total length of ROHs (kb)")+
+  ylab("Total length of ROHs (Mb)")+
   xlab("Region")+
   theme(legend.position="none", text=element_text(family="serif"))+
   scale_fill_manual(values = c("#ABD9E9", "#4575B4","#A50026"))+
   scale_x_discrete(labels=labels)+
-  scale_y_continuous(breaks = round(seq(min(set$KB), max(set$KB), by = 10000),1))
+  scale_y_continuous(breaks = round(seq(min(set$KB), max(set$KB), by = 10),1))
+
   
 bar_KB
+ggsave("roh_plot_50kb_colour_violin_WNA_JM.png", width=4.5, height=3, dpi=600)
+
+#library(patchwork)
+#roh + bar_NSEG / bar_KB
+#ggsave("roh_plots_100kb_50snp_5miss_colour.png", width=8, height=5, dpi=300)
+
+#bar_NSEG + bar_KB
+#ggsave("roh_plots_100kb_50snp_5miss_colour_barsonly.png", width=7, height=3.5, dpi=400)
+
+
