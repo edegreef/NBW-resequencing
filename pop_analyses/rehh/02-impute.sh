626090dfc9a1cd14c4a0b2d6f0edcc0e991d18a0
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2022-11-29 13:24:35 -0600

    Update 02-impute.sh

--- pop_analyses/rehh/02-impute.sh
+++ pop_analyses/rehh/02-impute.sh
@@ -2,7 +2,7 @@
 
 cd /home/degreefe/NBW/snps_2M/rehh/impute
 
-# Filter out the singleton snps
+# Filter out the single snp scaffolds
 vcftools --vcf NBW2_SNPS_2M.filter1.miss.biallel.ID.autosomes.SV.37.miss01.mac.min50kb.vcf --exclude-positions single_snp_windows_min50kb_CHROM_POS --recode --recode-INFO-all --out NBW2_SNPS_2M.filter1.miss.biallel.ID.autosomes.SV.37.miss01.mac.min50kb.rmsingles
 
 mv NBW2_SNPS_2M.filter1.miss.biallel.ID.autosomes.SV.37.miss01.mac.min50kb.rmsingles.recode.vcf NBW2_SNPS_2M.filter1.miss.biallel.ID.autosomes.SV.37.miss01.mac.min50kb.rmsingles.vcf
